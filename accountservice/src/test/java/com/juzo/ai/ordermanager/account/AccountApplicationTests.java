cb89a540844142f4f33fe839e7aa10c902bcbb9c
Zoltan Juhasz <zoltan_juhasz@epam.com>
2026-05-02 22:09:37 -0400

    Adding dummy test for CI flow

---
+++ accountservice/src/test/java/com/juzo/ai/ordermanager/account/AccountApplicationTests.java
@@ -0,0 +1,12 @@
+package com.juzo.ai.ordermanager.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
